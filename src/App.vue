987bcc67087ff8240ec30af553c753b4946791bb
James <james.lee@biji.co>
2020-12-14 11:47:57 +0800

    feat: router

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,30 @@
 <template>
-    <Header />
-    <Container :countries="state.countries" />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
-<script>
-import { defineAsyncComponent, reactive } from "vue";
-import axios from "axios";
-
-const components = {
-    Header: defineAsyncComponent(() => import("@/components/Header.vue")),
-    Container: defineAsyncComponent(() => import("@/layout/Container.vue")),
-};
-
-export default {
-    name: "App",
-    components,
-    setup() {
-        let state = reactive({
-            countries: [],
-        });
-        const countiresAPI = "DEU;USA;BRA;ISL;AFG;ALA;ALB;DZA";
-        const url = `https://restcountries.eu/rest/v2/alpha?codes=${countiresAPI}`;
-        axios.get(url).then((res) => (state.countries = res.data));
-
-        return {
-            state,
-        };
-    },
-};
-</script>
-
 <style lang="scss">
-@import "@/assets/scss/all.scss";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
