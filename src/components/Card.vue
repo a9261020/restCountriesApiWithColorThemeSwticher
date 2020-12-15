dec9d7b3a677a6d5b65338262a919cb243688ec6
James <james.lee@biji.co>
2020-12-15 10:39:21 +0800

    fix: detail中可以用tag去抓取其他資料

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -21,47 +21,47 @@
 
     <div class="card" v-show="isDetail">
         <div class="card-img">
-            <img :src="country?.flag" alt="" />
+            <img :src="state?.country?.flag" alt="" />
         </div>
         <div class="card-item">
-            <h2 class="card-item-title">{{ country?.name }}</h2>
+            <h2 class="card-item-title">{{ state?.country?.name }}</h2>
             <p>
                 Native Name:
-                <span>{{ country?.nativeName }}</span>
+                <span>{{ state?.country?.nativeName }}</span>
             </p>
             <p>
                 Population:
-                <span>{{ country?.population }}</span>
+                <span>{{ state?.country?.population }}</span>
             </p>
             <p>
                 Region:
-                <span>{{ country?.region }}</span>
+                <span>{{ state?.country?.region }}</span>
             </p>
             <p>
                 Sub Region:
-                <span>{{ country?.subregion }}</span>
+                <span>{{ state?.country?.subregion }}</span>
             </p>
             <p>
                 Capital:
-                <span>{{ country?.capital }}</span>
+                <span>{{ state?.country?.capital }}</span>
             </p>
             <p>
                 Top Level Domain:
-                <span>{{ country?.topLevelDomain?.[0] }}</span>
+                <span>{{ state?.country?.topLevelDomain?.[0] }}</span>
             </p>
             <p>
                 Currencies:
-                <span>{{ country?.currencies?.[0]?.name }}</span>
+                <span>{{ state?.country?.currencies?.[0]?.name }}</span>
             </p>
             <p>
                 Languages:
-                <span>{{ country?.languages?.[0]?.name }}</span>
+                <span>{{ state?.country?.languages?.[0]?.name }}</span>
             </p>
             <div class="border">
                 <h3>Border Countries</h3>
                 <div class="border-list">
                     <router-link
-                        v-for="country in border"
+                        v-for="country in state.border"
                         :key="country"
                         :to="`/detail/${country}`"
                     >
@@ -74,7 +74,9 @@
 </template>
 
 <script>
-import { defineAsyncComponent, computed } from "vue";
+import { defineAsyncComponent, computed, reactive, watchEffect } from "vue";
+import { useRoute } from "vue-router";
+import axios from "axios";
 
 const components = {
     Tag: defineAsyncComponent(() => import("@/components/Tag.vue")),
@@ -89,10 +91,23 @@ export default {
         isDetail: Boolean,
     },
     setup(props) {
-        const border = computed(() => props?.country?.borders?.slice(0, 3));
+        const route = useRoute();
+        const state = reactive({
+            country: props.country,
+            border: computed(() => state?.country?.borders?.slice(0, 3)),
+        });
+
+        watchEffect(() => {
+            const alpha3Code = route.params.alpha3Code;
+
+            if (alpha3Code) {
+                const url = `https://restcountries.eu/rest/v2/alpha/${alpha3Code}`;
+                axios.get(url).then((res) => (state.country = res.data));
+            }
+        });
 
         return {
-            border,
+            state,
         };
     },
 };
